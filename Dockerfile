32cc1fc4fb3fb4e9eb5fc39348c62a46932b5390
Brandon Lim <brandonlim.lim@gmail.com>
2019-11-23 23:31:34 +1100

    Migrate wkhtmltopdf packaging to Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM amazonlinux:2
+
+RUN yum install -y \
+    rpmdevtools \
+    wget
+
+ENV LAYER_DIR="/layer"
+ENV LAYER_ZIP="layer.zip"
+ENV TEMP_DIR="/tmp"
+ENV WKHTMLTOPDF_BIN="$TEMP_DIR/wkhtmltopdf.rpm"
+
+# Download wkhtmltopdf and its dependencies
+RUN wget -O $WKHTMLTOPDF_BIN https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox-0.12.5-1.centos7.$(arch).rpm \
+    && yum install --downloadonly --downloaddir=$TEMP_DIR $WKHTMLTOPDF_BIN
+
+# Extract all rpm files
+RUN cd $TEMP_DIR && rpmdev-extract *rpm
+
+# Copy wkhtmltopdf binary and dependency libraries for packaging
+RUN mkdir -p $LAYER_DIR/{bin,lib} \
+    && cp $TEMP_DIR/wkhtml*/usr/local/bin/* $LAYER_DIR/bin \
+    && cp $TEMP_DIR/*/usr/lib64/* $LAYER_DIR/lib || :
+
+# Zip files
+RUN cd $LAYER_DIR \
+    && zip -r $LAYER_ZIP bin lib \
+    && mv $LAYER_ZIP /
--- build.sh
+++ build.sh
@@ -2,35 +2,12 @@
 
 set -e
 
-LAYER_DIR="/layer"
+DOCKER_IMAGE_TAG="wkhtmltopdf:amazonlinux"
 LAYER_ZIP="layer.zip"
-TEMP_DIR="/tmp"
 
-DOCKER_IMAGE="amazonlinux:latest"
-CONTAINER=$(docker run -d -it $DOCKER_IMAGE)
+rm -f $LAYER_ZIP 1>/dev/null
 
-ARCHITECTURE=$(docker exec -it $CONTAINER arch | tr -d '\r\n')
-WKHTMLTOPDF_URL="https://downloads.wkhtmltopdf.org/0.12/0.12.5/wkhtmltox-0.12.5-1.centos7.$ARCHITECTURE.rpm"
-WKHTMLTOPDF_BIN="$TEMP_DIR/wkhtmltopdf.rpm"
-
-# Download wkhtmltopdf.rpm
-docker exec -it $CONTAINER \
-    /bin/bash -c \
-    "yum install -y wget \
-    && wget -O ${WKHTMLTOPDF_BIN} ${WKHTMLTOPDF_URL}"
-
-# Download and extract dependencies
-docker exec -it $CONTAINER yum install --downloadonly --downloaddir=$TEMP_DIR $WKHTMLTOPDF_BIN
-docker exec -it $CONTAINER yum install -y rpmdevtools
-docker exec -it $CONTAINER /bin/bash -c "cd $TEMP_DIR && rpmdev-extract *rpm"
-
-# Copy wkhtmltopdf binary and dependency libraries to directory for packaging
-docker exec -it $CONTAINER mkdir -p $LAYER_DIR/{bin,lib}
-docker exec -it $CONTAINER /bin/bash -c "cp $TEMP_DIR/wkhtml*/usr/local/bin/* $LAYER_DIR/bin"
-docker exec -it $CONTAINER /bin/bash -c "cp $TEMP_DIR/*/usr/lib64/* $LAYER_DIR/lib || :"
-# Compress files to reduce packaging size
-docker exec -it $CONTAINER /bin/bash -c "cd $LAYER_DIR && zip -r $LAYER_ZIP bin lib"
-
-docker cp $CONTAINER:/$LAYER_DIR/$LAYER_ZIP $LAYER_ZIP
-
+docker build -t $DOCKER_IMAGE_TAG - <Dockerfile
+CONTAINER=$(docker run -d $DOCKER_IMAGE_TAG)
+docker cp $CONTAINER:/$LAYER_ZIP $LAYER_ZIP
 docker rm -f $CONTAINER
