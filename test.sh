9d3fed4068636af9d9ec15edb446972c3d5d4573
Brandon Lim <brandonlim.lim@gmail.com>
2019-12-08 21:58:30 +1100

    Refactor common commands into function

--- test.sh
+++ test.sh
@@ -12,6 +12,40 @@ fi
 
 rm -rf $LAYER_DIR && unzip $LAYER_ZIP -d $LAYER_DIR 1>/dev/null
 
+# Invoke lambda function with wkhtmltopdf layer
+invoke_lambda_function() {
+    while [[ "$#" -gt 0 ]]; do
+        case $1 in
+        -c | --code)
+            local CODE_DIR="$2"
+            shift
+            ;;
+        -h | --handler)
+            local HANDLER="$2"
+            shift
+            ;;
+        -r | --runtime)
+            local RUNTIME="$2"
+            shift
+            ;;
+        *)
+            echo "Unknown parameter: $1" >&2
+            return 1
+            ;;
+        esac
+        shift
+    done
+
+    printf "%s\n" "Runtime: $RUNTIME"
+    docker run --rm \
+        -v "$CODE_DIR":/var/task:ro,delegated \
+        -v "$LAYER_DIR":/opt:ro,delegated \
+        lambci/lambda:$RUNTIME \
+        $HANDLER
+    if [ $? -ne 0 ]; then ERRORS+=1; fi
+    printf "\n"
+}
+
 # Test layer on:
 #   operating system: Amazon Linux, runtime: Java 8
 #   operating system: Amazon Linux 2, runtime: Java 11
@@ -22,14 +56,10 @@ JAVA_DIR="$PWD/tests/java"
 rm -rf "$JAVA_DIR/build" && docker run --rm -v "$JAVA_DIR":/app -w /app gradle:6.0 gradle build -q
 
 for runtime in $JAVA_RUNTIMES; do
-    printf "%s\n" "Runtime: $runtime"
-    docker run --rm \
-        -v "$JAVA_DIR/build/package":/var/task:ro,delegated \
-        -v "$LAYER_DIR":/opt:ro,delegated \
-        lambci/lambda:$runtime \
-        example.Example::handleRequest
-    if [ $? -ne 0 ]; then ERRORS+=1; fi
-    printf "\n"
+    invoke_lambda_function \
+        --code "$JAVA_DIR/build/package" \
+        --handler "example.Example::handleRequest" \
+        --runtime $runtime
 done
 
 # Test layer on:
@@ -39,14 +69,10 @@ done
 NODEJS_RUNTIMES="nodejs8.10 nodejs10.x nodejs12.x"
 
 for runtime in $NODEJS_RUNTIMES; do
-    printf "%s\n" "Runtime: $runtime"
-    docker run --rm \
-        -v "$PWD/tests/nodejs":/var/task:ro,delegated \
-        -v "$LAYER_DIR":/opt:ro,delegated \
-        lambci/lambda:$runtime \
-        index.handler
-    if [ $? -ne 0 ]; then ERRORS+=1; fi
-    printf "\n"
+    invoke_lambda_function \
+        --code "$PWD/tests/nodejs" \
+        --handler "index.handler" \
+        --runtime $runtime
 done
 
 # Test layer on:
@@ -56,14 +82,10 @@ done
 PYTHON_RUNTIMES="python3.6 python3.7 python3.8"
 
 for runtime in $PYTHON_RUNTIMES; do
-    printf "%s\n" "Runtime: $runtime"
-    docker run --rm \
-        -v "$PWD/tests/python":/var/task:ro,delegated \
-        -v "$LAYER_DIR":/opt:ro,delegated \
-        lambci/lambda:$runtime \
-        lambda_function.lambda_handler
-    if [ $? -ne 0 ]; then ERRORS+=1; fi
-    printf "\n"
+    invoke_lambda_function \
+        --code "$PWD/tests/python" \
+        --handler "lambda_function.lambda_handler" \
+        --runtime $runtime
 done
 
 rm -rf $LAYER_DIR
